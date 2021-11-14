cf022a35fe2f509ba4972a5aac6b4d6f8687b685
Batikan <batikanor@gmail.com>
2021-11-14 18:00:57 +0300

    restructured

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+// get reference to canvas
 const canvas = document.getElementsByTagName('canvas')[0]
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+//const canvas = document.getElementById('canvas')
+//const canvas = document.querySelector("#canvas") 
 
-const ctx = canvas.getContext('2d')
-ctx.fillText("Hey", 100, 100)
-ctx.fillRect(20,90,40,50)
+// get reference to rendering context
+const ctx = canvas.getContext('2d'); 
+
+// when window is first loaded
+window.addEventListener("load", () => {
+    console.log("Window has been loaded.");
+
+    // disableScroll()
+
+
+
+    // resizing
+    resizeCanvas()
+
+
+    
+    ctx.fillText("Hey", 100, 100)
+    ctx.fillRect(20,90,40,50)
+
+    
+
+
+    ctx.translate(100,100)
+    renderAvatar()
+    ctx.translate(0,40)
+    renderShot()
+
+})
 
 function renderAvatar(){ 
-    
+        
     // draw avatar shape
     ctx.beginPath()
     ctx.arc(0,0,22,0, (3/2) * Math.PI)
@@ -20,8 +46,9 @@ function renderAvatar(){
     ctx.fillStyle = 'black'
     ctx.fillText('batikanor', 0, -25)
 
+    // this is where rotation should go
 
-    // setting location of eyes (for direction)
+    // setting location of eyes and mouth (for direction)
     ctx.beginPath()
     ctx.moveTo(-5, 5)
     ctx.lineTo(-5, 17)
@@ -33,6 +60,27 @@ function renderAvatar(){
     ctx.strokeStyle = "#FFFFFF"; 
     ctx.stroke()
 }
+
+window.addEventListener("resize",resizeCanvas)
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth * 90 / 100
+    canvas.height = window.innerHeight * 90 / 100
+    console.log("Resized canvas to: " + canvas.width + " x " + canvas.height)
+}
+
+// thanks geeksforgeeks
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
 // thanks markE and Andrei Volgin
 function drawStar(cx,cy,spikes,outerRadius,innerRadius){
     var rot=Math.PI/2*3;
@@ -70,7 +118,3 @@ function renderShot(){
     // ctx.fill()
     drawStar(0,0,5,4,2)
 }
-ctx.translate(100,100)
-renderAvatar()
-ctx.translate(0,40)
-renderShot()
